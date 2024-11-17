a02a2a173165fc4eba1910e233fb4be731935b1c
samira dorchalian <samiradorchalain1998@gmail.com>
2024-11-17 18:30:01 +0330

    create layoutPage & its styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
+import Layout from "./layout/Layout";
 import ProductsPage from "./pages/ProductsPage";
 import DetailsPage from "./pages/DetailsPage";
 import CheckoutPage from "./pages/CheckoutPage";
@@ -12,13 +13,15 @@ function App() {
   return (
     <CartProvider>
       <ProductsProvider>
-        <Routes>
-          <Route index element={<Navigate to="/products" replace />} />
-          <Route path="/products" element={<ProductsPage />} />
-          <Route path="/products/:id" element={<DetailsPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="/*" element={<PageNotFound />} />
-        </Routes>
+        <Layout>
+          <Routes>
+            <Route index element={<Navigate to="/products" replace />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/products/:id" element={<DetailsPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="/*" element={<PageNotFound />} />
+          </Routes>
+        </Layout>
       </ProductsProvider>
     </CartProvider>
   );
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,5 +1,29 @@
-function Layout() {
-  return <div>Layout</div>;
+import { Link } from "react-router-dom";
+import { PiShoppingCartBold } from "react-icons/pi";
+
+import styles from "./Layout.module.css";
+import { useCart } from "../context/CartProvider";
+
+function Layout({ children }) {
+  const [state] = useCart();
+
+  return (
+    <>
+      <header className={styles.header}>
+        <Link to="/products">Shop-react</Link>
+        <Link to="/checkout">
+          <div>
+            <PiShoppingCartBold />
+            {!!state.itemsCounter && <span>{state.itemsCounter}</span>}
+          </div>
+        </Link>
+      </header>
+      {children}
+      <footer className={styles.footer}>
+        <p>Developed by Samira with 🌻</p>
+      </footer>
+    </>
+  );
 }
 
 export default Layout;
